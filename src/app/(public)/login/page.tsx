0f065593e18bebe51924c965ad761a3b71d97424
Lucas Wendell <lucaswhendellf@gmail.com>
2023-07-25 10:54:41 -0300

    style: create first structure from login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Login: React.FC = () => {
-	return <h1 className="text-red-700">ola Login</h1>;
+	return (
+		<main className="flex w-screen h-screen">
+			<section className="bg-blue-300 basis-9/12">
+				<h1 className="text-red-500">LOGO</h1>
+			</section>
+			<aside className="grow">
+				<figure className="bg-blue-700 w-full h-full">figure</figure>
+			</aside>
+		</main>
+	);
 };
 
 export default Login;
