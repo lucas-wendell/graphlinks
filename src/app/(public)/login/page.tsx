eb1fc18e7f5c6114bb5e428c407bdfef5e844f32
Lucas Wendell <lucaswhendellf@gmail.com>
2023-07-25 11:13:32 -0300

    style: add logo

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Login: React.FC = () => {
 	return (
 		<main className="flex w-screen h-screen">
-			<section className="bg-blue-300 basis-9/12">
-				<h1 className="text-red-500">LOGO</h1>
+			<section className="bg-blue-300 basis-9/12 p-5 relative">
+				<Image
+					className="absolute"
+					src={'/assets/logo.svg'}
+					alt="graphlinks logo"
+					width={120}
+					height={120}
+				/>
 			</section>
 			<aside className="grow">
 				<figure className="bg-blue-700 w-full h-full">figure</figure>
