4353ed6afd648a331febe8c3634557977e9f4116
Lucas Wendell <lucaswhendellf@gmail.com>
2023-07-26 16:13:19 -0300

    docs: change stories from input from login page

--- src/app/(public)/components/Input/Input.stories.tsx
+++ src/app/(public)/components/Input/Input.stories.tsx
@@ -5,10 +5,6 @@ export default {
 	component: Input,
 	title: 'Form/Input',
 	tags: ['autodocs'],
-	args: {
-		placeholder: 'Text Input',
-		type: 'text',
-	},
 	argTypes: {
 		type: {
 			options: ['text', 'password'],
