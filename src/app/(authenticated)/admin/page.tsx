29fdd509089350af1f65417860459e6e8e05858b
Lucas Wendell <lucaswhendellf@gmail.com>
2023-08-03 11:10:31 -0300

    refactor: change redirect from admin page

--- src/app/(authenticated)/admin/page.tsx
+++ src/app/(authenticated)/admin/page.tsx
@@ -1,15 +1,6 @@
-import { authOptions } from '@/utils/authOptions';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
 import React from 'react';
 
-const Admin: React.FC = async () => {
-	const session = await getServerSession(authOptions);
-
-	if (!session) {
-		redirect('/login');
-	}
-
+const Admin: React.FC = () => {
 	return <h1>admin</h1>;
 };
 
