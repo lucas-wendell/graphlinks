6f643b094c559b380957a6d2f1f738984d9ff4c9
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-12 15:25:50 -0300

    style: create first style from Navbar component

--- src/app/(authenticated)/components/Navbar/Navbar.tsx
+++ src/app/(authenticated)/components/Navbar/Navbar.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+import logoSquare from '../../../../../public/assets/logo-square.svg';
 export type Props = {};
 
 const Navbar: React.FC<Props> = ({}) => {
-	return <nav>ola</nav>;
+	return (
+		<div className="flex items-center justify-between w-full py-4 px-5 rounded-full bg-ghost-gray">
+			<div className="">
+				<Image src={logoSquare} width={40} height={40} alt="logo icon" />
+				<nav></nav>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
