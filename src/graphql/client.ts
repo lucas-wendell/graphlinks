61611afb3ef34cf9b30e8536af1e9eff288f1eb4
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-15 18:45:11 -0300

    docs: add documentation to graphql path

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,5 +1,9 @@
 import { GraphQLClient } from 'graphql-request';
 
+/**
+ * Variable to initialize graphql client with graphql-request library
+ * @summary Variable to initialize graphql client with graphql-request library
+ *  */
 export const client = new GraphQLClient(
 	`${process.env.NEXT_PUBLIC_API_URL}/graphql`,
 );
--- src/graphql/mutations/reset-password.ts
+++ src/graphql/mutations/reset-password.ts
@@ -1,6 +1,19 @@
 import { gql } from 'graphql-request';
 import { GQL_FRAGMENT_USER } from '../fragments/user';
 
+/** 
+ * Variable to create graphql query mutation with graphql-request
+ * @summary Variable to create graphql query mutation with graphql-request 
+ * @param {string} code - Recovery code 
+ * @param {string} password - New password 
+ * @param {string} passwordConfirmation - Password confirmation 
+ * @description Query mutation to reset user password. This query mutation receive three arguments (code, password and password Confirmation) and must be used like example below:
+ * @example // Example:
+ * const data = await client.request<{ resetPassword: CredentialsResponse }>(
+		GQL_MUTATION_RESET_PASSWORD,
+		{ code: 'recoverycode123', password: 'example123', passwordConfirmation: 'example123' },
+	);
+ * */
 export const GQL_MUTATION_RESET_PASSWORD = gql`
 	${GQL_FRAGMENT_USER}
 
