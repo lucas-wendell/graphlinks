b52fad75fe4778723a35ebf9548c8c2cd0a4e5f2
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-15 18:14:34 -0300

    docs: create docs to servide path

--- src/service/forgot-password.ts
+++ src/service/forgot-password.ts
@@ -2,6 +2,13 @@ import { client } from '@/graphql/client';
 import { GQL_MUTATION_FORGOT_PASSWORD } from '@/graphql/mutations/forgot-password';
 import { Response } from './types/forgot-password-types';
 
+/**
+ * Function to send email to user for recovery password
+ * @summary Function to send email to user for recovery password
+ * @async
+ * @function resetPassword
+ * @param {string} email - User recovery email
+ */
 export const forgotPassword = async (
 	email: string,
 ): Promise<{ isOk: boolean }> => {
