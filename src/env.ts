7448480be8691d94a70a7e5d850ecbd884001980
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-15 18:14:34 -0300

    docs: create docs to service path

--- src/env.ts
+++ src/env.ts
@@ -11,4 +11,11 @@ const envSchema = z.object({
 	GITHUB_CLIENT_SECRET: z.string().min(1),
 });
 
-export const env = envSchema.parse(process.env);
+/**
+ *  This is a variable with all environment variables,
+ *  to use it you must declare your environment variable in a
+ * .env file at the root folder of project.
+ * And add a schema at envSchema variable located in src/env.ts with the same name of your environment variable.
+ * @summary A variable with all environment variables
+ */
+export const env: z.infer<typeof envSchema> = envSchema.parse(process.env);
