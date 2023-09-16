835704b4ac98028ee784bb4e5c557b2452d571dd
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-16 17:34:54 -0300

    feat: create page form template component

---
+++ src/app/(public)/components/PageFormTemplate/PageFormTemplate.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+type PageFormTemplateProps = {
+	children: ReactNode;
+};
+
+export const PageFormTemplate: React.FC<PageFormTemplateProps> = ({
+	children,
+}) => (
+	<main className="flex min-w-screen min-h-screen overflow-hidden">
+		<section className="flex flex-col justify-center items-center bg-ghost-gray basis-9/12 max-sm:basis-full relative gap-10 p-10 max-lg:basis-8/12">
+			<Image
+				className="left-10 top-10 absolute"
+				src="/assets/logo.svg"
+				alt="graphlinks logo"
+				width={120}
+				height={120}
+				placeholder="blur"
+				blurDataURL="/assets/logo.svg"
+			/>
+			{children}
+		</section>
+		<aside className="max-sm:hidden grow">
+			<figure className="flex items-center justify-center bg-electric-indigo w-full h-full"></figure>
+		</aside>
+	</main>
+);
