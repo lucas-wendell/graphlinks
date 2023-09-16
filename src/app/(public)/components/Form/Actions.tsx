9f2bf2d3d8148a78b578f0c9b963902d89468920
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-16 17:26:14 -0300

    feat: add prop to extends actions styles in form

--- src/app/(public)/components/Form/Actions.tsx
+++ src/app/(public)/components/Form/Actions.tsx
@@ -1,9 +1,11 @@
 import { type ReactNode } from 'react';
 
-type ActionsProps = { children: ReactNode };
+type ActionsProps = { children: ReactNode; moreStyles?: string };
 
-const Actions: React.FC<ActionsProps> = ({ children }) => (
-	<div className="flex flex-col items-center justify-center gap-4 mt-8">
+const Actions: React.FC<ActionsProps> = ({ children, moreStyles }) => (
+	<div
+		className={`flex flex-col items-center justify-center gap-4 mt-8 ${moreStyles}`}
+	>
 		{children}
 	</div>
 );
