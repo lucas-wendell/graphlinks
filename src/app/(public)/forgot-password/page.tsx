592b69c46c60251c716614e236c59d6a5dd79bfd
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-16 17:42:12 -0300

    feat: implements page form template component

--- src/app/(public)/forgot-password/page.tsx
+++ src/app/(public)/forgot-password/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import ForgotPasswordForm from '../components/ForgotPasswordForm/Form';
+import { PageFormTemplate } from '../components/PageFormTemplate/PageFormTemplate';
 
 export type ForgotPasswordProps = {
 	searchParams: Record<string, string | null | undefined>;
@@ -10,31 +10,15 @@ const ForgotPassword: React.FC<ForgotPasswordProps> = async ({
 	searchParams,
 }) => {
 	return (
-		<>
-			<main className="flex min-w-screen min-h-screen overflow-hidden">
-				<section className="flex flex-col justify-center items-center bg-ghost-gray basis-9/12 max-sm:basis-full max-sm:p-10 relative gap-10 p-20 max-lg:basis-8/12">
-					<Image
-						className="left-10 top-10 absolute"
-						src="/assets/logo.svg"
-						alt="graphlinks logo"
-						width={120}
-						height={120}
-						placeholder="blur"
-						blurDataURL="/assets/logo.svg"
-					/>
-					<div className="max-sm:text-center w-full text-jet">
-						<h1 className="font-bold text-xl ">Forget something?</h1>
-						<p className="text-sm">
-							Enter your email to receive password reset instructions.
-						</p>
-					</div>
-					<ForgotPasswordForm />
-				</section>
-				<aside className="max-sm:hidden grow">
-					<figure className="flex items-center justify-center bg-electric-indigo w-full h-full"></figure>
-				</aside>
-			</main>
-		</>
+		<PageFormTemplate>
+			<div className="max-sm:text-center w-full text-jet">
+				<h1 className="font-bold text-xl ">Forget something?</h1>
+				<p className="text-sm">
+					Enter your email to receive password reset instructions.
+				</p>
+			</div>
+			<ForgotPasswordForm />
+		</PageFormTemplate>
 	);
 };
 
--- src/app/(public)/forgot-password/recovery-code/page.tsx
+++ src/app/(public)/forgot-password/recovery-code/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Image from 'next/image';
 
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import RecoveryCodeForm from '../../components/RecoveryCodeForm/Form';
+import { PageFormTemplate } from '../../components/PageFormTemplate/PageFormTemplate';
 
 export type RecoveryCodeProps = {
 	searchParams: Record<string, string | null | undefined>;
@@ -17,31 +17,13 @@ const RecoveryCode: React.FC<RecoveryCodeProps> = async ({ searchParams }) => {
 	if (!email) redirect('/forgot-password');
 
 	return (
-		<>
-			<main className="flex min-w-screen min-h-screen overflow-hidden">
-				<section className="flex flex-col justify-center items-center bg-ghost-gray basis-9/12 max-sm:basis-full max-sm:p-10 relative gap-10 p-20 max-lg:basis-8/12">
-					<Image
-						className="left-10 top-10 absolute"
-						src="/assets/logo.svg"
-						alt="graphlinks logo"
-						width={120}
-						height={120}
-						placeholder="blur"
-						blurDataURL="/assets/logo.svg"
-					/>
-					<div className="max-sm:text-center w-full text-jet">
-						<h1 className="font-bold text-xl ">Enter your recovery code</h1>
-						<p className="text-sm">
-							Enter your recovery code to reset password.
-						</p>
-					</div>
-					<RecoveryCodeForm />
-				</section>
-				<aside className="max-sm:hidden grow">
-					<figure className="flex items-center justify-center bg-electric-indigo w-full h-full"></figure>
-				</aside>
-			</main>
-		</>
+		<PageFormTemplate>
+			<div className="max-sm:text-center w-full text-jet">
+				<h1 className="font-bold text-xl ">Enter your recovery code</h1>
+				<p className="text-sm">Enter your recovery code to reset password.</p>
+			</div>
+			<RecoveryCodeForm />
+		</PageFormTemplate>
 	);
 };
 
