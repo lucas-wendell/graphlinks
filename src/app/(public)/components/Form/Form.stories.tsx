710c2cd5e63841597c38253d695d0307f8280c5c
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-17 12:22:43 -0300

    docs: create a better doc for form component

--- src/app/(public)/components/Form/Form.stories.tsx
+++ src/app/(public)/components/Form/Form.stories.tsx
@@ -1,9 +1,12 @@
-import { Meta, StoryObj } from '@storybook/react';
-import Form from './Form';
+import { Meta } from '@storybook/react';
+import { FormComposition } from '.';
+
+import Button from '../Button/Button';
+import Input from '../Input/Input';
 
 export default {
-	component: Form,
-	title: 'PublicPages/Components/LoginForm',
+	component: FormComposition.Form,
+	title: 'PublicPages/Components/BaseForm',
 	tags: ['autodocs'],
 	parameters: {
 		nextjs: {
@@ -12,6 +15,57 @@ export default {
 	},
 } as Meta;
 
-type Story = StoryObj;
+export const Base = () => (
+	<FormComposition.Form>
+		<FormComposition.Inputs>
+			<Input<{ test: string }> type="text" placeholder="example" name="test" />
+			<Input<{ password: string }>
+				type="password"
+				placeholder="Password example"
+				name="password"
+			/>
+			<Input<{ test: string }>
+				type="text"
+				placeholder="example"
+				name="test"
+				error={'With Error'}
+			/>
+		</FormComposition.Inputs>
+		<FormComposition.Form>
+			<FormComposition.Actions>
+				<Button text="Action Example 1"></Button>
+				<Button text="Action Example 2" styleType="secondary"></Button>
+				<Button text="Action Example 3" styleType="secondary"></Button>
+			</FormComposition.Actions>
+		</FormComposition.Form>
+	</FormComposition.Form>
+);
 
-export const Primary: Story = {};
+export const Inputs = () => (
+	<FormComposition.Form>
+		<FormComposition.Inputs>
+			<Input<{ test: string }> type="text" placeholder="example" name="test" />
+			<Input<{ password: string }>
+				type="password"
+				placeholder="Password example"
+				name="password"
+			/>
+			<Input<{ test: string }>
+				type="text"
+				placeholder="example"
+				name="test"
+				error={'With Error'}
+			/>
+		</FormComposition.Inputs>
+	</FormComposition.Form>
+);
+
+export const Actions = () => (
+	<FormComposition.Form>
+		<FormComposition.Actions>
+			<Button text="Action Example 1"></Button>
+			<Button text="Action Example 2" styleType="secondary"></Button>
+			<Button text="Action Example 3" styleType="secondary"></Button>
+		</FormComposition.Actions>
+	</FormComposition.Form>
+);
