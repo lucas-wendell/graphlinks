80e23c92a21eb260cfc616682a223e34514dc74d
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-23 17:31:56 -0300

    style: change toggle button style

--- src/app/(authenticated)/components/ToggleButton/ToggleButton.tsx
+++ src/app/(authenticated)/components/ToggleButton/ToggleButton.tsx
@@ -6,18 +6,19 @@ export type Props = {};
 
 const ToggleButton: React.FC<Props> = ({}) => {
 	const [isActive, setIsActive] = useState(false);
-
 	return (
 		<motion.button
 			onClick={() => setIsActive(prev => !prev)}
 			className={`flex ${isActive ? 'justify-end' : 'justify-start'} bg-${
-				isActive ? 'dark-spring-green' : 'french-gray'
-			} rounded-full py-1 px-1 h-max w-14 transition-colors`}
+				isActive ? 'dark-spring-green' : 'white'
+			} rounded-full p-1 h-max w-12 transition-colors`}
 		>
 			<motion.span
 				layout
 				transition={spring}
-				className="w-5 h-5 rounded-full bg-ghost-gray"
+				className={`w-4 h-4 rounded-full bg-${
+					isActive ? 'ghost-gray' : 'dark-spring-green'
+				}`}
 			></motion.span>
 		</motion.button>
 	);
