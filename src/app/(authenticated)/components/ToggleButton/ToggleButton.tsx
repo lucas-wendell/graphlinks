68b8083311acdd07bef45a0f72d2b8d45e1f71fd
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-26 14:50:58 -0300

    style: change color from toggle button component

--- src/app/(authenticated)/components/ToggleButton/ToggleButton.tsx
+++ src/app/(authenticated)/components/ToggleButton/ToggleButton.tsx
@@ -9,16 +9,14 @@ const ToggleButton: React.FC<Props> = ({}) => {
 	return (
 		<motion.button
 			onClick={() => setIsActive(prev => !prev)}
-			className={`flex ${isActive ? 'justify-end' : 'justify-start'} bg-${
-				isActive ? 'dark-spring-green' : 'white'
+			className={`flex ${isActive ? 'justify-end' : 'justify-start'} ${
+				isActive ? 'bg-dark-spring-green' : 'bg-jet'
 			} rounded-full p-1 h-max w-12 transition-colors`}
 		>
 			<motion.span
 				layout
 				transition={spring}
-				className={`w-4 h-4 rounded-full bg-${
-					isActive ? 'ghost-gray' : 'dark-spring-green'
-				}`}
+				className="w-4 h-4 rounded-full bg-ghost-gray"
 			></motion.span>
 		</motion.button>
 	);
