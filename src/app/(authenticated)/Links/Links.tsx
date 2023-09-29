2206c246925525b8d4a8e3a21fb162dadcfc84bb
Lucas Wendell <lucaswhendellf@gmail.com>
2023-09-29 16:19:12 -0300

    feat: create links component

---
+++ src/app/(authenticated)/Links/Links.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from '../components/Link/Link';
+
+const Links: React.FC = () => {
+	return (
+		<div className="flex flex-col gap-4 w-full">
+			<Link />
+			<Link />
+			<Link />
+			<Link />
+			<Link />
+		</div>
+	);
+};
+
+export default Links;
