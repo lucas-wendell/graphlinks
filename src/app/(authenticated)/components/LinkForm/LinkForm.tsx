335b57b6f0b98d5cd85b22da71bdaa00b2d65027
Lucas Wendell <lucaswhendellf@gmail.com>
2023-10-15 17:07:01 -0300

    refactor: create custom hook to LinkForm component

--- src/app/(authenticated)/components/LinkForm/LinkForm.tsx
+++ src/app/(authenticated)/components/LinkForm/LinkForm.tsx
@@ -3,54 +3,33 @@
 import React, { useRef } from 'react';
 import LinkInput from '../LinkInput/LinkInput';
 
-import { z } from 'zod';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import type { LinkFormData } from './schema';
+import { useLinkForm } from './hooks/useLinkForm';
 
 export type Props = {};
 
-export const linkFormSchema = z.object({
-	link: z.string().nonempty('You must provide a link'),
-	name: z.string().nonempty('You must indentifier your link'),
-});
-
-export type LinkFormData = z.infer<typeof linkFormSchema>;
-
 const LinkForm: React.FC<Props> = ({}) => {
 	const formRef = useRef<null | HTMLFormElement>(null);
-
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<LinkFormData>({
-		mode: 'onBlur',
-		reValidateMode: 'onChange',
-		resolver: zodResolver(linkFormSchema),
-	});
-
-	const onSubmit = ({ name, link }: LinkFormData) => {
-		console.log({ name, link });
-	};
+	const { errors, onSubmit, register } = useLinkForm();
 
 	return (
 		<form
 			ref={formRef}
-			onSubmit={handleSubmit(onSubmit)}
+			onSubmit={onSubmit}
 			className="flex flex-col gap-2 max-[345px]:gap-1"
 		>
 			<LinkInput<LinkFormData>
 				register={register}
 				error={errors.name?.message}
 				name="name"
-				registerOptions={{ onBlur: handleSubmit(onSubmit) }}
+				registerOptions={{ onBlur: onSubmit }}
 			/>
 			<LinkInput<LinkFormData>
 				register={register}
 				error={errors.link?.message}
 				name="link"
 				link="https://github.com"
-				registerOptions={{ onBlur: handleSubmit(onSubmit) }}
+				registerOptions={{ onBlur: onSubmit }}
 			/>
 		</form>
 	);
