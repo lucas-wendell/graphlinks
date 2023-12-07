3848dec53a747d0dffc9fd98b440f76ad6f93d66
Lucas Wendell <lucaswhendellf@gmail.com>
2023-12-07 16:11:12 -0300

    feat: add components to appearance page

--- src/app/(authenticated)/admin/appearance/page.tsx
+++ src/app/(authenticated)/admin/appearance/page.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { ConfigBlockComposition } from '../../components/ConfigBlock';
+import BackgroundPreview from '../../components/BackgroundPreview/BackgroundPreview';
+import ColorForm from '../../components/ColorForm/ColorForm';
+import LinksPreview, {
+	Props,
+} from '../../components/LinksPreview/LinksPreview';
 
 const Appearance: React.FC = () => {
-	return <h1>ola</h1>;
+	const linksTypes: Array<Props['type']> = [
+		'fill',
+		'outline',
+		'softShadow',
+		'hardShadow',
+	];
+
+	return (
+		<>
+			<ConfigBlockComposition.Root name="backgrounds">
+				<ConfigBlockComposition.Content>
+					<BackgroundPreview />
+				</ConfigBlockComposition.Content>
+
+				<ConfigBlockComposition.ColorForms>
+					<ColorForm name="background color" />
+				</ConfigBlockComposition.ColorForms>
+			</ConfigBlockComposition.Root>
+
+			<ConfigBlockComposition.Root name="buttons">
+				<ConfigBlockComposition.Content>
+					{linksTypes.map((type, index) => (
+						<LinksPreview key={`${type}-${index}`} type={type} />
+					))}
+				</ConfigBlockComposition.Content>
+
+				<ConfigBlockComposition.ColorForms>
+					<ColorForm name="button color" />
+					<ColorForm name="button font color" />
+				</ConfigBlockComposition.ColorForms>
+			</ConfigBlockComposition.Root>
+		</>
+	);
 };
 
 export default Appearance;
